f189297def3155cd9e1b4f67357db413a50c3570
Michela <Michela@DESKTOP-D2IQQ4H>
2021-11-25 22:26:54 +0100

    First Commit/ComplexNumber class

---
+++ ScientificCalculator/src/it/unisa/SE/project/ComplexNumber.java
@@ -0,0 +1,138 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package it.unisa.SE.project;
+import java.lang.Math.*;
+import java.lang.ArithmeticException;
+/**
+ *
+ * @author Michela
+ */
+public class ComplexNumber {
+    private double real;
+    private double imaginary;
+    
+    public ComplexNumber(double real, double imaginary) {
+        this.real = real;
+        this.imaginary = imaginary;
+    }
+
+    public double getReal() {
+        return real;
+    }
+
+    public double getImaginary() {
+        return imaginary;
+    }
+  
+   
+public void setReal(Double real) {
+        this.real = real;
+    }
+
+  
+    public void setImaginary(Double imaginary) {
+        this.imaginary = imaginary;
+    }
+    
+    @Override
+    public String toString() {
+        if(imaginary>0)
+            return String.valueOf(real) + " + j" + String.valueOf(imaginary);
+        else if(imaginary<0)
+            return String.valueOf(real) + " - j" + String.valueOf(-imaginary);
+        else if( imaginary == 0)
+             return String.valueOf(real); 
+        else 
+            return String.valueOf(imaginary);
+                
+    }
+    
+ @Override
+    public int hashCode() {
+        int hash = 7;
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final ComplexNumber other = (ComplexNumber) obj;
+        if (Double.doubleToLongBits(this.real) != Double.doubleToLongBits(other.real)) {
+            return false;
+        }
+        if (Double.doubleToLongBits(this.imaginary) != Double.doubleToLongBits(other.imaginary)) {
+            return false;
+        }
+        return true;
+    }
+    
+ public static ComplexNumber sub(ComplexNumber number1, ComplexNumber number2){
+        double r1=number1.getReal();
+        double i1=number1.getImaginary();
+
+        double r2=number2.getReal();
+        double i2=number2.getImaginary();
+
+        double sumReal=r1-r2;
+        double sumIm=i1-i2;
+
+        ComplexNumber complexResult=new ComplexNumber(sumReal, sumIm);
+        return complexResult;
+    }
+
+   
+    
+     /** 
+     * return the module of a complex number
+     * @param number1
+     * @return double
+     */
+    public static double mod(ComplexNumber number1){
+        double r1=number1.getReal();
+        double i1=number1.getImaginary();
+        double mod=Math.sqrt(r1*r1+i1*i1);
+        return mod;
+    }
+    
+    /** 
+     * return the argument of a complex number
+     * @param number1
+     * @return double
+     * @throws ArithmeticException
+     */
+    public static double arg(ComplexNumber number1) throws ArithmeticException{
+        double r=number1.getReal();
+        double i=number1.getImaginary();
+        
+        if(r==0 && i>0){
+            return (Math.PI/2*100d);
+        }
+        if(r==0 && i<0){
+            return (-Math.PI/2);
+        }
+        if(r==0 && i==0){
+            throw new ArithmeticException("Arg of 0 doesn't exist");
+        }
+        if(r<0 && i>=0){
+            return (Math.atan(i/r)+Math.PI);
+        }
+        if(r<0 && i<0){
+            return (Math.atan(i/r)-Math.PI);
+        } 
+        
+        return (Math.atan(i/r));
+    }
+    
+    
+     
+}
---
+++ ScientificCalculator/test/it/unisa/SE/project/test/ComplexNumberTest.java
@@ -0,0 +1,96 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4TestClass.java to edit this template
+ */
+package it.unisa.SE.project.test;
+
+import it.unisa.SE.project.ComplexNumber;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+import org.junit.*;
+
+/**
+ *
+ * @author Michela
+ */
+public class ComplexNumberTest {
+public ComplexNumberTest() {
+    }
+     private ComplexNumber number1;
+    private ComplexNumber number2;
+    private ComplexNumber number3;
+    private ComplexNumber number4;
+    private ComplexNumber number5;
+    private ComplexNumber number6;
+    private ComplexNumber number7;
+    
+    
+    @Before
+    public void setUp() {
+        number1 = new ComplexNumber(7.0, 15.0);
+        number2 = new ComplexNumber(-3.0, 9.0);
+        number3 = new ComplexNumber(11.0, -6.0);
+        number4 = new ComplexNumber(20.5, 0.0);
+        number5 = new ComplexNumber(0.0, -7.5);
+        number6 = new ComplexNumber(-6.5, -15.5);
+        number7 = new ComplexNumber(0.0, 0.0);
+        
+    }
+
+  
+     @Test
+    public void testSub() {
+        ComplexNumber expected12 = new ComplexNumber(10.0, 6.0);
+        ComplexNumber expected13 = new ComplexNumber(7.0, 22.5);
+        ComplexNumber expected14 = new ComplexNumber(7.0,15.0 );
+        ComplexNumber expected15 = new ComplexNumber(-9.5,-6 );
+        ComplexNumber expected16 = new ComplexNumber(6.5, 8.0);
+        
+        assertEquals(expected12, ComplexNumber.sub(number1, number2));
+        assertEquals(expected13, ComplexNumber.sub(number1, number5));
+        assertEquals(expected14, ComplexNumber.sub(number1, number7));
+        assertEquals(expected15, ComplexNumber.sub(number3, number4));
+        assertEquals(expected16, ComplexNumber.sub(number5, number6));
+    }
+
+   
+   
+    @Test
+    public void testToString() {
+        String expected = "5.0 + j9.0";
+        ComplexNumber n = new ComplexNumber(5.0, 9.0);
+        assertEquals(expected, n.toString());
+
+        expected = "0.0 + j9.0";
+        n = new ComplexNumber(0.0, 9.0);
+        assertEquals(expected, n.toString());
+
+        expected = "5.0";
+        n = new ComplexNumber(5.0, 0.0);
+        assertEquals(expected, n.toString());
+
+        expected = "3.5 - j7.9";
+        n.setReal(3.5);
+        n.setImaginary(-7.9);
+        assertEquals(expected, n.toString());
+
+        expected = "-11.5 - j10.9";
+        n.setReal(-11.5);
+        n.setImaginary(-10.9);
+        assertEquals(expected, n.toString());
+
+        expected = "-3.0 + j5.7";
+        n.setReal(-3.0);
+        n.setImaginary(5.7);
+        assertEquals(expected, n.toString());
+
+    }
+    
+    
+
+}
