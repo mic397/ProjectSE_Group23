7060a6fca2605400ca3869fb7962292827034705
aniel <aniel@host.docker.internal>
2021-11-26 18:03:40 +0100

    Added tests for conjugate, scale, invertSign

--- ScientificCalculator/test/it/unisa/SE/project/test/ComplexNumberTest.java
+++ ScientificCalculator/test/it/unisa/SE/project/test/ComplexNumberTest.java
@@ -5,11 +5,11 @@
 package it.unisa.SE.project.test;
 
 import ProjectException.ArgumentNotDefinedException;
+import ProjectException.notAcceptableValueException;
 import it.unisa.SE.project.ComplexNumber;
 
 import static org.junit.Assert.*;
 import org.junit.*;
-import java.lang.ArithmeticException;
 
 /**
  *
@@ -29,7 +29,6 @@ public class ComplexNumberTest {
     private ComplexNumber number8;
     private ComplexNumber number9;
     private ComplexNumber number10;
-    private ComplexNumber number11;
 
     @Before
     public void setUp() {
@@ -42,7 +41,7 @@ public class ComplexNumberTest {
         number7 = new ComplexNumber(0.0, 0.0);
         number8 = new ComplexNumber(1, 1);
         number9 = new ComplexNumber(2, 1);
-        number10= new ComplexNumber(1,0);
+        number10 = new ComplexNumber(1, 0);
     }
 
     @Test
@@ -76,8 +75,6 @@ public class ComplexNumberTest {
         assertEquals(expected16, ComplexNumber.sub(number5, number6));
     }
 
-    
-
     @Test
     public void testToString() {
         String expected = "5.0 + j9.0";
@@ -108,147 +105,240 @@ public class ComplexNumberTest {
         assertEquals(expected, n.toString());
 
     }
-    
-    @Test
-    public void testArg() throws ArgumentNotDefinedException{
-        double arg1=ComplexNumber.arg(number1);
-        double arg2=ComplexNumber.arg(number2);
-        double arg3=ComplexNumber.arg(number3);
-        double arg4=ComplexNumber.arg(number4);
-        double arg5=ComplexNumber.arg(number5);
-        double arg6=ComplexNumber.arg(number6);
-        
 
-        double expected1=1.134;
-        double expected2=1.892;
-        double expected3=-0.499;
-        double expected4=0;
-        double expected5=-1.570;
-        double expected6=-1.968;
-              
-                
-        double delta=0.5;
-        
-        assertEquals(expected1,Math.round(arg1*1000d)/1000d,delta);
-        assertEquals(expected2,Math.round(arg2*1000d)/1000d,delta);
-        assertEquals(expected3,Math.round(arg3*1000d)/1000d,delta);
-        assertEquals(expected4,Math.round(arg4*1000d)/1000d,delta);
-        assertEquals(expected5,Math.round(arg5*1000d)/1000d,delta);
-        assertEquals(expected6,Math.round(arg6*1000d)/1000d,delta);
-        
-        assertTrue(arg1<=Math.PI);
-        assertTrue(arg1>-Math.PI);
-        assertTrue(arg2<=Math.PI);
-        assertTrue(arg2>-Math.PI);
-        assertTrue(arg3<=Math.PI);
-        assertTrue(arg3>-Math.PI);
-        assertTrue(arg4<=Math.PI);
-        assertTrue(arg4>-Math.PI);
-        assertTrue(arg5<=Math.PI);
-        assertTrue(arg5>-Math.PI);
-        assertTrue(arg6<=Math.PI);
-        assertTrue(arg6>-Math.PI);
+    @Test
+    public void testArg() throws ArgumentNotDefinedException {
+        double arg1 = ComplexNumber.arg(number1);
+        double arg2 = ComplexNumber.arg(number2);
+        double arg3 = ComplexNumber.arg(number3);
+        double arg4 = ComplexNumber.arg(number4);
+        double arg5 = ComplexNumber.arg(number5);
+        double arg6 = ComplexNumber.arg(number6);
+
+        double expected1 = 1.134;
+        double expected2 = 1.892;
+        double expected3 = -0.499;
+        double expected4 = 0;
+        double expected5 = -1.570;
+        double expected6 = -1.968;
+
+        double delta = 0.5;
+
+        assertEquals(expected1, Math.round(arg1 * 1000d) / 1000d, delta);
+        assertEquals(expected2, Math.round(arg2 * 1000d) / 1000d, delta);
+        assertEquals(expected3, Math.round(arg3 * 1000d) / 1000d, delta);
+        assertEquals(expected4, Math.round(arg4 * 1000d) / 1000d, delta);
+        assertEquals(expected5, Math.round(arg5 * 1000d) / 1000d, delta);
+        assertEquals(expected6, Math.round(arg6 * 1000d) / 1000d, delta);
+
+        assertTrue(arg1 <= Math.PI);
+        assertTrue(arg1 > -Math.PI);
+        assertTrue(arg2 <= Math.PI);
+        assertTrue(arg2 > -Math.PI);
+        assertTrue(arg3 <= Math.PI);
+        assertTrue(arg3 > -Math.PI);
+        assertTrue(arg4 <= Math.PI);
+        assertTrue(arg4 > -Math.PI);
+        assertTrue(arg5 <= Math.PI);
+        assertTrue(arg5 > -Math.PI);
+        assertTrue(arg6 <= Math.PI);
+        assertTrue(arg6 > -Math.PI);
     }
 
     @Test
-    public void testMod(){
-        double mod1=ComplexNumber.mod(number1);
-        double mod2=ComplexNumber.mod(number2);
-        double mod3=ComplexNumber.mod(number3);
-        double mod4=ComplexNumber.mod(number4);
-        double mod5=ComplexNumber.mod(number5);
-        double mod6=ComplexNumber.mod(number6);
-        double mod7=ComplexNumber.mod(number7);
+    public void testMod() {
+        double mod1 = ComplexNumber.mod(number1);
+        double mod2 = ComplexNumber.mod(number2);
+        double mod3 = ComplexNumber.mod(number3);
+        double mod4 = ComplexNumber.mod(number4);
+        double mod5 = ComplexNumber.mod(number5);
+        double mod6 = ComplexNumber.mod(number6);
+        double mod7 = ComplexNumber.mod(number7);
 
-        double expected1=16.552;
-        double expected2=9.486;
-        double expected3=12.529;
-        double expected4=20.5;
-        double expected5=7.5;
-        double expected6=16.807;
-        double expected7=0;
-        
-        double delta=0.5;
-        
+        double expected1 = 16.552;
+        double expected2 = 9.486;
+        double expected3 = 12.529;
+        double expected4 = 20.5;
+        double expected5 = 7.5;
+        double expected6 = 16.807;
+        double expected7 = 0;
 
-        assertEquals(expected1,Math.round(mod1*1000d)/1000d,delta);
-        assertEquals(expected2,Math.round(mod2*1000d)/1000d,delta);
-        assertEquals(expected3,Math.round(mod3*1000d)/1000d,delta);
-        assertEquals(expected4,Math.round(mod4*1000d)/1000d,delta);
-        assertEquals(expected5,Math.round(mod5*1000d)/1000d,delta);
-        assertEquals(expected6,Math.round(mod6*1000d)/1000d,delta);
-        assertEquals(expected7,Math.round(mod7*1000d)/1000d,delta);
+        double delta = 0.5;
+
+        assertEquals(expected1, Math.round(mod1 * 1000d) / 1000d, delta);
+        assertEquals(expected2, Math.round(mod2 * 1000d) / 1000d, delta);
+        assertEquals(expected3, Math.round(mod3 * 1000d) / 1000d, delta);
+        assertEquals(expected4, Math.round(mod4 * 1000d) / 1000d, delta);
+        assertEquals(expected5, Math.round(mod5 * 1000d) / 1000d, delta);
+        assertEquals(expected6, Math.round(mod6 * 1000d) / 1000d, delta);
+        assertEquals(expected7, Math.round(mod7 * 1000d) / 1000d, delta);
     }
 
-    @Test 
-    public void testSqrt() throws ArgumentNotDefinedException{ 
-        ComplexNumber complex1=ComplexNumber.sqrt(number1); 
-        ComplexNumber complex2=ComplexNumber.sqrt(number2); 
-        ComplexNumber complex3=ComplexNumber.sqrt(number3); 
-        ComplexNumber complex4=ComplexNumber.sqrt(number4); 
-        ComplexNumber complex5=ComplexNumber.sqrt(number5); 
- 
-        ComplexNumber expected1=new ComplexNumber(3.432, 2.186); 
-        ComplexNumber expected2=new ComplexNumber(1.801, 2.499); 
-        ComplexNumber expected3=new ComplexNumber(3.43, -0.875); 
-        ComplexNumber expected4=new ComplexNumber(4.528, 0.0); 
-        ComplexNumber expected5=new ComplexNumber(1.936, -1.936); 
- 
-        assertEquals(expected1,complex1); 
-        assertEquals(expected2,complex2); 
-        assertEquals(expected3,complex3); 
-        assertEquals(expected4,complex4); 
-        assertEquals(expected5,complex5); 
-    }
-   
-@Test 
-    public void testMul(){ 
-       
-        ComplexNumber expected1=new ComplexNumber(-156.0, 18.0); 
-        ComplexNumber expected2=new ComplexNumber(0.0, 0.0); 
-        ComplexNumber expected3=new ComplexNumber(-133.25,-317.75 ); 
-        ComplexNumber expected4=new ComplexNumber( -116.25, 48.75); 
-        ComplexNumber expected5=new ComplexNumber(7.0,15.0 ); 
-         
-         
-        assertEquals(expected1, ComplexNumber.mul(number1, number2)); 
-        assertEquals(expected2, ComplexNumber.mul(number1, number7)); 
-        assertEquals(expected3, ComplexNumber.mul(number4, number6)); 
-        assertEquals(expected4, ComplexNumber.mul(number5, number6)); 
-        assertEquals(expected5, ComplexNumber.mul(number1, number10)); 
-    }
-    
     @Test
-    public void testDiv(){
-        ComplexNumber expected1=new ComplexNumber(1.267,-1.2);
-        ComplexNumber actual1=ComplexNumber.div(number1, number2);
-        actual1.setReal(Math.round(actual1.getReal()*1000d)/1000d);
-        actual1.setImaginary(Math.round(actual1.getImaginary()*1000d)/1000d);
-        assertEquals(expected1,actual1);
-        
-       
-        ComplexNumber expected2=new ComplexNumber(-3.0,9.0);
-        ComplexNumber actual2=ComplexNumber.div(number2, number10);
-        actual2.setReal(Math.round(actual2.getReal()*1000d)/1000d);
-        actual2.setImaginary(Math.round(actual2.getImaginary()*1000d)/1000d);
-        assertEquals(expected2,actual2);
-        
-        
-        ComplexNumber expected3=new ComplexNumber(0.0,2.733);
-        ComplexNumber actual3=ComplexNumber.div(number4, number5);
-        actual3.setReal(Math.round(actual3.getReal()*1000d)/1000d);
-        actual3.setImaginary(Math.round(actual3.getImaginary()*1000d)/1000d);
-        assertEquals(expected3,actual3);
-        
-        ComplexNumber expected4=new ComplexNumber(0.412,0.173);
-        ComplexNumber actual4=ComplexNumber.div(number5, number6);
-        actual4.setReal(Math.round(actual4.getReal()*1000d)/1000d);
-        actual4.setImaginary(Math.round(actual4.getImaginary()*1000d)/1000d);
-        assertEquals(expected4,actual4);
-        
-        
-        assertThrows(ArithmeticException.class, () -> ComplexNumber.div(number1, number7));
+    public void testSqrt() throws ArgumentNotDefinedException {
+        ComplexNumber complex1 = ComplexNumber.sqrt(number1);
+        ComplexNumber complex2 = ComplexNumber.sqrt(number2);
+        ComplexNumber complex3 = ComplexNumber.sqrt(number3);
+        ComplexNumber complex4 = ComplexNumber.sqrt(number4);
+        ComplexNumber complex5 = ComplexNumber.sqrt(number5);
+
+        ComplexNumber expected1 = new ComplexNumber(3.432, 2.186);
+        ComplexNumber expected2 = new ComplexNumber(1.801, 2.499);
+        ComplexNumber expected3 = new ComplexNumber(3.43, -0.875);
+        ComplexNumber expected4 = new ComplexNumber(4.528, 0.0);
+        ComplexNumber expected5 = new ComplexNumber(1.936, -1.936);
+
+        assertEquals(expected1, complex1);
+        assertEquals(expected2, complex2);
+        assertEquals(expected3, complex3);
+        assertEquals(expected4, complex4);
+        assertEquals(expected5, complex5);
     }
 
+    /**
+     * /**
+     * Verify that the multiplication between a complex number with real and
+     * imaginary parts equal to zero and a complex number is null.
+     *
+     * Verify that the multiplication between a complex number and the neutral
+     * value (with real part equals to one and imaginary part equal to zero) is
+     * equal to the number itself
+     */
+    @Test
+    public void testMul() {
+        ComplexNumber expected1 = new ComplexNumber(-156.0, 18.0);
+        ComplexNumber expected2 = new ComplexNumber(0.0, 0.0);
+        ComplexNumber expected3 = new ComplexNumber(-133.25, -317.75);
+        ComplexNumber expected4 = new ComplexNumber(-116.25, 48.75);
+        ComplexNumber expected5 = new ComplexNumber(7.0, 15.0);
+
+        assertEquals(expected1, ComplexNumber.mul(number1, number2));
+        assertEquals(expected2, ComplexNumber.mul(number1, number7));
+        assertEquals(expected3, ComplexNumber.mul(number4, number6));
+        assertEquals(expected4, ComplexNumber.mul(number5, number6));
+        assertEquals(expected5, ComplexNumber.mul(number1, number10));
+    }
+
+    @Test
+    public void testDiv() {
+        ComplexNumber expected1 = new ComplexNumber(1.267, -1.2);
+        ComplexNumber actual1 = ComplexNumber.div(number1, number2);
+        actual1.setReal(Math.round(actual1.getReal() * 1000d) / 1000d);
+        actual1.setImaginary(Math.round(actual1.getImaginary() * 1000d) / 1000d);
+        assertEquals(expected1, actual1);
+
+        ComplexNumber expected2 = new ComplexNumber(-3.0, 9.0);
+        ComplexNumber actual2 = ComplexNumber.div(number2, number10);
+        actual2.setReal(Math.round(actual2.getReal() * 1000d) / 1000d);
+        actual2.setImaginary(Math.round(actual2.getImaginary() * 1000d) / 1000d);
+        assertEquals(expected2, actual2);
+
+        ComplexNumber expected3 = new ComplexNumber(0.0, 2.733);
+        ComplexNumber actual3 = ComplexNumber.div(number4, number5);
+        actual3.setReal(Math.round(actual3.getReal() * 1000d) / 1000d);
+        actual3.setImaginary(Math.round(actual3.getImaginary() * 1000d) / 1000d);
+        assertEquals(expected3, actual3);
+
+        ComplexNumber expected4 = new ComplexNumber(0.412, 0.173);
+        ComplexNumber actual4 = ComplexNumber.div(number5, number6);
+        actual4.setReal(Math.round(actual4.getReal() * 1000d) / 1000d);
+        actual4.setImaginary(Math.round(actual4.getImaginary() * 1000d) / 1000d);
+        assertEquals(expected4, actual4);
+    }
+
+    /**
+     * Verify that the real and imaginary parts of the denominator are not equal
+     * to zero -> Limit Case
+     */
+    @Test(expected = ArithmeticException.class)
+    public void testDivToZero() {
+        ComplexNumber.div(number1, number7);
+    }
+
+    /**
+     * Verify that the multiplication, between a complex number with real and
+     * imaginary parts equal to zero and a real number, is equal to complex
+     * number with real and imaginary parts equal to zero.
+     *
+     * Verify that the multiplication, between a complex number with real and
+     * imaginary parts not equal to zero and a real number equal to 1, is equal
+     * to originary complex number
+     */
+    @Test
+    public void testScale() {
+        double val = 0;
+        assertEquals(new ComplexNumber(0, 0), number1.scale(val));
+        val = 1;
+        assertEquals(number1, number1.scale(val));
+        val = 2;
+        number2.setReal(number1.getReal() * val);
+        number2.setImaginary(number1.getImaginary() * val);
+        assertEquals(number2, number1.scale(val));
+    }
+
+    /**
+     * Verify the corretness to calculate a complex number's conjugate
+     */
+    @Test //Conjugate
+    public void testConjugate() {
+        //re != 0 and im != 0 
+        number1.setReal(2.0);
+        number1.setImaginary(1.0);
+        assertEquals(new ComplexNumber(number1.getReal(), number1.getImaginary() * (-1)), ComplexNumber.conjugate(number1));
+        //re = 0 and im != 0
+        number1.setReal(0.0);
+        number1.setImaginary(1.0);
+        assertEquals(new ComplexNumber(number1.getReal(), number1.getImaginary() * (-1)), ComplexNumber.conjugate(number1));
+        //re != 0; im = 0
+        number1.setReal(1.0);
+        number1.setImaginary(0.0);
+        assertEquals(number1, ComplexNumber.conjugate(number1));
+    }
+
+    /**
+     * Verify that multplying by -1, it returns the original number
+     */
+    @Test
+    public void testInvertSign() {
+        number1.setReal(1.0);
+        number1.setImaginary(2.0);
+        assertEquals(number1.scale(-1), number1.invertSign());
+        assertEquals(number1, number1.invertSign().invertSign());
+    }
+
+    /**
+     * Verify the corretness to calculate a complex number's reciprocal
+     *
+     * @throws notAcceptableValueException
+     */
+    @Test(expected = notAcceptableValueException.class)
+    public void testReciprocalNull() throws notAcceptableValueException {
+        //Limit case
+        number1.setReal(0.0);
+        number1.setImaginary(0.0);
+        ComplexNumber.reciprocal(number1);
+    }
+
+    /**
+     * Verify if z = 0 + 0i; the reciprocal of z -> (1/z) is not defined
+     *
+     * @throws notAcceptableValueException
+     */
+    @Test
+    public void testReciprocal() throws notAcceptableValueException {
+        //re != 0 and im = 0 
+        number1.setReal(1.0);
+        number1.setImaginary(1.0);
+        number2.setReal(0.5);
+        number2.setImaginary(-0.5);
+        assertEquals(number2, ComplexNumber.reciprocal(number1));
+
+        number1.setReal(2.0);
+        number1.setImaginary(1.0);
+        number2.setReal(1.0);
+        number2.setImaginary(0.0);
+        ComplexNumber res = ComplexNumber.div(number2, number1);
+        assertEquals(res, ComplexNumber.reciprocal(number1));
+    }
 
 }
