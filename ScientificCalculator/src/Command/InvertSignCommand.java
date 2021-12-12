4be0425cfd2255450b43ac5b6e1f2ecd07d8ba47
aniel <aniel@LAPTOP-RKNI8U7L>
2021-12-12 17:25:25 +0100

    InvertSignCommand

--- ScientificCalculator/src/Command/InvertSignCommand.java
+++ ScientificCalculator/src/Command/InvertSignCommand.java
@@ -4,26 +4,32 @@
  * and open the template in the editor.
  */
 package Command;
+
+import ProjectException.UnderOneElementException;
+import ProjectException.stackIsEmptyException;
 import it.unisa.SE.project.*;
-
-
 
 /**
  *
  * @author Michela
  */
 public class InvertSignCommand implements Command {
+
     private Calculator c;
-    
-    public InvertSignCommand(Calculator c){
+
+    public InvertSignCommand(Calculator c) {
         this.c = c;
     }
-    
-    
+
+    /**
+     * 
+     * @return 
+     * @throws UnderOneElementException
+     * @throws stackIsEmptyException 
+     */
     @Override
-    public void execute() throws Exception {
+    public void execute() throws UnderOneElementException, stackIsEmptyException {
         c.invertSign();
-        
     }
-    
+
 }
