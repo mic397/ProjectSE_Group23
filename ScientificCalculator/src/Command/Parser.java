3d305fd9002da0e63e27ab9cc63b467b4e904e5c
aniel <aniel@LAPTOP-RKNI8U7L>
2021-12-12 23:54:45 +0100

    Parser commit: added comments

--- ScientificCalculator/src/Command/Parser.java
+++ ScientificCalculator/src/Command/Parser.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Command;
+
 import ProjectException.ElementNotAvailableException;
 import it.unisa.SE.project.*;
 
@@ -10,39 +11,43 @@ import ProjectException.notAcceptableValueException;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 /**
  *
  * @author Michela
  */
 public class Parser {
+
     ParserString parse;
     Calculator cal;
-    
-    public Parser(){
-       parse = new ParserString(); 
-       cal = Calculator.getCalculator();
+
+    public Parser() {
+        parse = new ParserString();
+        cal = Calculator.getCalculator();
     }
- 
-    
-     public List<Command> parser(String operation,CommandInvoker inv) throws notAcceptableValueException, ElementNotAvailableException{
-       List<Command> listCommand = new ArrayList<>();
-       List<String> operationList = Arrays.asList(operation.split("\\s+"));
-       for (String tmp : operationList){
+
+    public List<Command> parser(String operation, CommandInvoker inv) throws notAcceptableValueException, ElementNotAvailableException {
+        List<Command> listCommand = new ArrayList<>();
+        List<String> operationList = Arrays.asList(operation.split("\\s+"));
+        for (String tmp : operationList){
+            String ret = this.parse.checkComplexNumber(tmp);
+                if(tmp.equals(ParserString.getInvalid_insert()))
                 if (!inv.map.containsKey(tmp)){
                     throw new ElementNotAvailableException("In this operation is present a signature not found before");
-                }
+                } 
             }
-       for(int i=0; i < operationList.size(); i++){
-           Command comm = inv.map.get(operationList.get(i));
-           if(comm==null){
-               ComplexNumber newNumber = parse.recognizeComplexNumber(operationList.get(i));
-               comm = new InsertCommand(cal,newNumber); 
-           }
-           listCommand.add(comm);
-          
-       }
-       
-       return listCommand;
+        
+        for (int i = 0; i < operationList.size(); i++) {
+            Command comm = inv.map.get(operationList.get(i));
+            if (comm == null) {
+                ComplexNumber newNumber = parse.recognizeComplexNumber(operationList.get(i));
+                comm = new InsertCommand(cal, newNumber);
+            }
+            listCommand.add(comm);
+
+        }
+
+        return listCommand;
 
     }
 
