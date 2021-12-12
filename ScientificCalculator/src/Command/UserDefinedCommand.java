c61475c8702476d620bc5d99cd5b6248c938d13f
aniel <aniel@LAPTOP-RKNI8U7L>
2021-12-12 17:28:19 +0100

    UserDefinedCommand added Exceptions

--- ScientificCalculator/src/Command/UserDefinedCommand.java
+++ ScientificCalculator/src/Command/UserDefinedCommand.java
@@ -3,34 +3,34 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Command;
+
 import it.unisa.SE.project.*;
 
 import java.util.List;
-import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
  * @author Michela
  */
+
 public class UserDefinedCommand implements Command {
-    public List<Command> commands;
+    private List<Command> commands;
     
-    
-    
-    public UserDefinedCommand(List<Command> commands){
-        
-        this.commands= commands;
+    public UserDefinedCommand(List<Command> commands) {
+        this.commands = commands;
     }
-    
-    
+
+    /**
+     * @throws java.lang.Exception
+     */
     @Override
-    public void execute() throws Exception {
-        for(Command c: commands){
-            c.execute(); 
-        }
+    public void execute() throws Exception{
+        LinkedList<ComplexNumber> tmp = new LinkedList<>();
+            for (Command c : commands) {
+                    c.execute();
+            } 
     }
-    
-    
-    
-    
 }
